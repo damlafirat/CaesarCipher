8f8f2e2eedba50a3cdc86500c5969cbf6c855fdf
Damla Firat <firatdamla93@gmail.com>
2018-11-14 12:25:27 +0300

    first commit

---
+++ CaesarCipher/Encryption.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaesarCipher
+{
+    class Encryption
+    {
+        public static string decrypt(string v1, int v2)
+        {
+            string result = "";
+
+            char[] arr = v1.ToLower().ToCharArray();
+
+            char temp;
+
+            foreach (var item in arr)
+            {
+                if (item >= 'a' && item <= 'z')
+                {
+                    temp = (char)(item - v2);
+
+                    if (temp > 'z')
+                        temp = (char)(temp - 26);
+
+                    else if (temp < 'a')
+                        temp = (char)(temp + 26);
+                }
+
+                else
+                    temp = item;
+
+                result += temp;
+            }
+
+            return result;
+        }
+
+        public static string encrypt(string v1, int v2)
+        {
+            string result = "";
+
+            char[] arr = v1.ToLower().ToCharArray();
+
+            char temp;
+
+            foreach (var item in arr)
+            {
+                if (item >= 'a' && item <= 'z')
+                {
+                   temp  = (char)(item + v2);
+
+                    if (temp > 'z')
+                        temp = (char)(temp - 26);
+
+                    else if (temp < 'a')
+                        temp = (char)(temp + 26);
+                }
+
+                else
+                    temp = item;
+
+                result += temp;
+            }
+            
+
+            return result;
+        }
+    }
+}
---
+++ CaesarCipher/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Encryption.encrypt("ABCDEFGHIKLMNOPQRSTVXYZ", 2));
+
+            Console.WriteLine(Encryption.decrypt("CDEFGHIKLMNOPQRSTVXYZAB", 2));
+
+            Console.ReadLine();
+        }
+    }
+}
